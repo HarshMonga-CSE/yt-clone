b54bf8184795e1459f06128c0eb68e4576c56f54
Harsh Monga <harshmonga64@gmail.com>
2025-05-16 21:27:56 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     require('autoprefixer')
+//   ]
+// }
+
+
+
+
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss').default,
+//     require('autoprefixer').default,
+//   ],
+// };
+
+
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-// vite.config.js
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 4173,
-  }
 })
-
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
